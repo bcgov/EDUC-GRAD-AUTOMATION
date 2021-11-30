23d167e21f4905c59bc6357f4b37d0e1141ab091
Ditcher <Chris.Ditcher@gov.bc.ca>
2021-11-30 10:03:01 -0800

    cleaning: Removing superfluous stuff.

--- frontend/tests/page_objects/loginPage.js
+++ frontend/tests/page_objects/loginPage.js
@@ -12,25 +12,15 @@ class loginPage {
     };
 
     async staffLogin(credentials, url) {
-        //for (let i = 0; i < 10; i++) {
-            try {
-                await t.typeText(this.username, credentials.username, { timeout: 20000 })
-                    .typeText(this.password, credentials.password, { timeout: 20000 })
-                    .click(this.submitButton);
-                await t.expect((this.penSearch).exists).ok({ timeout: 20000 });
-                log.info("Staff user successfully logged in:    " + credentials.username);
-                //break;
-            } catch (error) {
-                log.error;
-                //await t.eval(() => location.reload())
-                //log.warn("Element not found, Refreshing the page")
-
-                //await t.navigateTo(url)
-                //log.info("Navigating to student search")
-
-                //await t.expect((this.submitButton).exists).ok({ timeout: 20000 })
-            }
-        //}
+        try {
+            await t.typeText(this.username, credentials.username, { timeout: 20000 })
+                .typeText(this.password, credentials.password, { timeout: 20000 })
+                .click(this.submitButton);
+            await t.expect((this.penSearch).exists).ok({ timeout: 20000 });
+            log.info("Staff user successfully logged in:    " + credentials.username);
+        } catch (error) {
+            log.error;
+        }
     };
 
 };
