ac02c81e4ceb20f24e022c46468d34613b0f9f85
Ditcher <Chris.Ditcher@gov.bc.ca>
2021-12-21 08:50:40 -0800

    Created comparator object

---
+++ backend/grad-trax-test-suite/src/main/java/ca/bc/gov/educ/gtts/model/dto/TraxGradComparatorNonGradDto.java
@@ -0,0 +1,15 @@
+package ca.bc.gov.educ.gtts.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TraxGradComparatorNonGradDto {
+
+    String pen;
+    String nonGradCode;
+
+}
