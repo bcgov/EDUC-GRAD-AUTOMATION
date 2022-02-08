f96b1d73d44719bf8ec0e08ee1ee8e7082741df2
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2022-02-08 11:52:40 -0800

    Disabled autoconfig on security for endpoints

--- backend/grad-trax-comparison-test/src/main/java/ca/bc/gov/gradtraxcomparisontest/GradTraxComparisonTestApplication.java
+++ backend/grad-trax-comparison-test/src/main/java/ca/bc/gov/gradtraxcomparisontest/GradTraxComparisonTestApplication.java
@@ -4,10 +4,11 @@ import ca.bc.gov.gradtraxcomparisontest.services.CompareServiceTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class GradTraxComparisonTestApplication {
 
     @Autowired
---
+++ backend/grad-trax-comparison-test/src/main/java/ca/bc/gov/gradtraxcomparisontest/config/Config.java
@@ -0,0 +1,12 @@
+package ca.bc.gov.gradtraxcomparisontest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+@EnableOAuth2Client
+@Configuration
+public class Config {
+
+
+
+}
