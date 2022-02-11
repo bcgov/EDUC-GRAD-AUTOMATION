9ea03938fbd31573db8252fe62e8827d5bfcb282
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2022-02-11 11:34:15 -0800

    Removed testing apps

--- backend/grad-trax-test-client/src/main/java/ca/bc/gov/gradtraxtestclient/repository/TraxTswTranDemogRepository.java
+++
@@ -1,7 +0,0 @@
-package ca.bc.gov.gradtraxtestclient.repository;
-
-import ca.bc.gov.gradtraxtestclient.model.entity.TswTranDemogEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TraxTswTranDemogRepository extends CrudRepository<TswTranDemogEntity, String> {
-}
