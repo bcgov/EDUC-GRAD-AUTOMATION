4da3b41b93c476aa3f2de104486533083a4f80d6
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2022-02-15 08:44:37 -0800

    Added test to confirm not found.

--- backend/rest-demo/src/test/java/ca/bc/gov/restdemo/RestDemoApplicationTests.java
+++ backend/rest-demo/src/test/java/ca/bc/gov/restdemo/RestDemoApplicationTests.java
@@ -1,13 +1,36 @@
 package ca.bc.gov.restdemo;
 
+import ca.bc.gov.restdemo.controllers.RestDemoController;
+import ca.bc.gov.restdemo.services.RestDemoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+@WebMvcTest(RestDemoController.class)
 class RestDemoApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    RestDemoService restDemoService;
+
     @Test
-    void contextLoads() {
+    void testGetReturnsNotFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/" + UUID.randomUUID()).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 
 }
