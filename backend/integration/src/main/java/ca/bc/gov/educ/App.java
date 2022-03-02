574f4909ba90e07c269af754c723474ddd460962
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2022-03-02 13:41:37 -0800

    Added intrgration skeleton.

---
+++ backend/integration/src/main/java/ca/bc/gov/educ/App.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.educ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This application has no code! Just test cases. See README for full details." );
+    }
+}
---
+++ backend/integration/src/test/java/ca/bc/gov/educ/GradCourseAPITests.java
@@ -0,0 +1,10 @@
+package ca.bc.gov.educ;
+
+/**
+ * Integration testing for grad-course-api
+ */
+public class GradCourseAPITests {
+
+    
+
+}
