028d4526d366de7a610b055e20664f4d6af3e1f7
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2022-03-09 15:46:30 -0800

    Code cleanup

--- backend/grad-main-test-suite/src/test/java/ca/bc/gov/educ/gmts/integration/GradProgramAPITests.java
+++ backend/grad-main-test-suite/src/test/java/ca/bc/gov/educ/gmts/integration/GradProgramAPITests.java
@@ -2,12 +2,18 @@ package ca.bc.gov.educ.gmts.integration;
 
 import ca.bc.gov.educ.gmts.config.RequiredProperties;
 import ca.bc.gov.educ.gmts.config.TestConfig;
+import ca.bc.gov.educ.gmts.model.programapi.GraduationProgramCode;
 import ca.bc.gov.educ.gmts.utils.URLUtils;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.net.URISyntaxException;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.testng.Assert.assertTrue;
@@ -32,13 +38,16 @@ public class GradProgramAPITests {
     }
 
     /**
-     * Tests /api/v1/program/gradrequirementtype
+     * Tests /api/v1/program/programs
      */
     @Test
-    public void testGetGradRequirementType() throws URISyntaxException {
+    public void testGetAllPrograms() throws URISyntaxException {
         String url = URLUtils.getURL(RequiredProperties.STUDENT_PROGRAM_API_URL, PROGRAMS_PATH, null);
-        given().auth().oauth2(TOKEN).get(url).then().statusCode(200);
-        assertTrue(true);
+        GraduationProgramCode[] codes = given().auth()
+                .oauth2(TOKEN)
+                .get(url)
+                .as(GraduationProgramCode[].class);
+        Assert.assertTrue(codes.length > 0);
     }
 
 }
--- backend/grad-main-test-suite/src/test/java/ca/bc/gov/educ/gmts/utils/URLUtils.java
+++ backend/grad-main-test-suite/src/test/java/ca/bc/gov/educ/gmts/utils/URLUtils.java
@@ -13,6 +13,14 @@ import java.util.Map;
 
 public class URLUtils {
 
+    /**
+     * Helper method for building urls
+     * @param requiredProperties a base url, for example RequiredProperties.STUDENT_COURSE_API_URL (required)
+     * @param path the path portion of url, for example "api/v1/endpoint"
+     * @param params optional params map
+     * @return
+     * @throws URISyntaxException
+     */
     public static String getURL(RequiredProperties requiredProperties, String path, Map<String, String> params) throws URISyntaxException {
         UriComponentsBuilder builder = UriComponentsBuilder.fromUri(
                 new URI(TestConfig.getInstance().getApiEndPoint(requiredProperties)));
@@ -20,12 +28,17 @@ public class URLUtils {
             builder.path(path);
         }
         if(params != null && !params.isEmpty()){
-            builder.queryParams(convertMaptToMultiValueMap(params));
+            builder.queryParams(convertMapToMultiValueMap(params));
         }
         return builder.build().toString();
     }
 
-    private static MultiValueMap<String, String> convertMaptToMultiValueMap(Map<String, String> params){
+    /**
+     * Internal helper
+     * @param params
+     * @return
+     */
+    private static MultiValueMap<String, String> convertMapToMultiValueMap(Map<String, String> params){
         MultiValueMap<String, String> p = new LinkedMultiValueMap<>();
         for (Map.Entry<String, String> entry : params.entrySet()) {
             p.add(entry.getKey(), entry.getValue());
