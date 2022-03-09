ca9d5b9efab50abbff79ddac73467bf730ebbb07
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2022-03-09 13:02:16 -0800

    Added a convenience url utils class

--- backend/grad-main-test-suite/src/test/java/ca/bc/gov/educ/gmts/integration/GradCourseAPITests.java
+++ backend/grad-main-test-suite/src/test/java/ca/bc/gov/educ/gmts/integration/GradCourseAPITests.java
@@ -8,10 +8,15 @@ import ca.bc.gov.educ.gmts.model.courseapi.CourseRestriction;
 import ca.bc.gov.educ.gmts.model.courseapi.CourseRestrictions;
 import ca.bc.gov.educ.gmts.services.GenericHTTPRequestService;
 import ca.bc.gov.educ.gmts.services.GenericHTTPRequestServiceImpl;
+import ca.bc.gov.educ.gmts.utils.URLUtils;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.testng.Assert.assertTrue;
 
@@ -21,6 +26,7 @@ import static org.testng.Assert.assertTrue;
 public class GradCourseAPITests {
 
     GenericHTTPRequestService requestService;
+    private static final String COURSE_RESTRICTION_PATH = "/api/v1/course/course-restriction";
 
     @BeforeClass
     public void setup() {
@@ -39,16 +45,23 @@ public class GradCourseAPITests {
      * Tests /api/v1/course/course-restriction
      */
     @Test
-    public void testGetCourseRestrictionsWithParams_ExpectModel() throws GenericHTTPRequestServiceException, NotFoundException {
-        String mainCourse = "ACSF";
-        String mainCourseLevel = "11";
-        String url = TestConfig.getInstance().getApiEndPoint(RequiredProperties.STUDENT_COURSE_API_URL) + "/api/v1/course/course-restriction?courseCode=" + mainCourse + "&courseLevel=" + mainCourseLevel;
+    public void testGetCourseRestrictionsWithParams_ExpectModel() throws GenericHTTPRequestServiceException, NotFoundException, URISyntaxException {
+        String courseCode = "courseCode";
+        String courseLevel = "courseLevel";
+        Map<String, String> params = new HashMap<>(){
+            {
+                put(courseCode, "ACSF");
+                put(courseLevel, "11");
+            }
+        };
+        String url = URLUtils.getURL(RequiredProperties.STUDENT_COURSE_API_URL, COURSE_RESTRICTION_PATH, params);
         CourseRestrictions restrictions = requestService.get(url, CourseRestrictions.class);
         if(restrictions != null && !restrictions.getCourseRestrictionList().isEmpty()){
             CourseRestriction restriction = restrictions.getCourseRestrictionList().get(0);
             assertTrue(
-                    restriction.getMainCourse().equalsIgnoreCase(mainCourse) &&
-                    restriction.getMainCourseLevel().equalsIgnoreCase(mainCourseLevel));
+                    restriction.getMainCourse().equalsIgnoreCase(params.get(courseCode)) &&
+                    restriction.getMainCourseLevel().equalsIgnoreCase(params.get(courseLevel))
+            );
         } else {
             Assert.fail();
         }
@@ -63,6 +76,31 @@ public class GradCourseAPITests {
         String url = TestConfig.getInstance().getApiEndPoint(RequiredProperties.STUDENT_COURSE_API_URL) + "/api/v1/course/course-restriction";
         CourseRestrictions restrictions = requestService.get(url, CourseRestrictions.class);
         assertTrue(restrictions != null && restrictions.getCourseRestrictionList().size() > 0);
+    }
+
+    @Test
+    public void testGetCourseRestrictionByRestrictedCourseCodeAndLevel_ExpectObject() throws URISyntaxException, GenericHTTPRequestServiceException, NotFoundException {
+        Map<String, String> params = new HashMap<>(){
+            {
+                put("courseCode", "GRPR");
+                put("courseLevel", "11");
+                put("restrictedCourseCode", "GRPRF");
+                put("restrictedCourseLevel", "11");
+            }
+        };
+        String url = URLUtils.getURL(RequiredProperties.STUDENT_COURSE_API_URL, COURSE_RESTRICTION_PATH, params);
+        CourseRestrictions restrictions = requestService.get(url, CourseRestrictions.class);
+        if(restrictions != null && !restrictions.getCourseRestrictionList().isEmpty()){
+            CourseRestriction restriction = restrictions.getCourseRestrictionList().get(0);
+            assertTrue(
+                    restriction.getMainCourse().equalsIgnoreCase(params.get("courseCode")) &&
+                            restriction.getMainCourseLevel().equalsIgnoreCase(params.get("courseLevel")) &&
+                            restriction.getRestrictedCourse().equalsIgnoreCase(params.get("restrictedCourseCode")) &&
+                            restriction.getRestrictedCourseLevel().equalsIgnoreCase(params.get("restrictedCourseLevel"))
+            );
+        } else {
+            Assert.fail();
+        }
     }
 
 }
---
+++ backend/grad-main-test-suite/src/test/java/ca/bc/gov/educ/gmts/utils/URLUtils.java
@@ -0,0 +1,38 @@
+package ca.bc.gov.educ.gmts.utils;
+
+import ca.bc.gov.educ.gmts.config.RequiredProperties;
+import ca.bc.gov.educ.gmts.config.TestConfig;
+
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+public class URLUtils {
+
+    public static String getURL(RequiredProperties requiredProperties, String path, Map<String, String> params) throws URISyntaxException {
+        String base = TestConfig.getInstance().getApiEndPoint(requiredProperties);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUri(
+                new URI(TestConfig.getInstance().getApiEndPoint(RequiredProperties.STUDENT_COURSE_API_URL)));
+        if(path != null && path.length() > 0){
+            builder.path(path);
+        }
+        if(params != null && !params.isEmpty()){
+            builder.queryParams(convertMaptToMultiValueMap(params));
+        }
+        return builder.build().toString();
+    }
+
+    private static MultiValueMap<String, String> convertMaptToMultiValueMap(Map<String, String> params){
+        MultiValueMap<String, String> p = new LinkedMultiValueMap<>();
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            p.add(entry.getKey(), entry.getValue());
+        }
+        return p;
+    }
+
+
+}
